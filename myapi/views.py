8ba81461d33b8efea8216810fac42dfa0128fd21
yoonjyjy <diniless@naver.com>
2019-10-30 23:42:21 +0900

    first commit

--- myapi/views.py
+++ myapi/views.py
@@ -7,13 +7,41 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 
+# 검색 기능
+from rest_framework.filters import SearchFilter
+
+# 커스텀 페이지네이션
+from rest_framework.pagination import PageNumberPagination
+
+# 이름은 마음대로, 커스텀이지만 첫번째 방식을 상속받아옴
+class MyPagination(PageNumberPagination) :
+    page_size = 3
+
 class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Essay.objects.all()
     serializer_class = EssaySerializer
+    pagination_class = MyPagination
+
+    filter_backends = [SearchFilter]
+    search_fields = ('title', 'body')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def get_queryset(self) :
+        qs = super().get_queryset()
+
+        if self.request.user.is_authenticated :
+            # 여기서 is_staff는 사실 permission이다!
+            if self.request.user.is_staff :
+                pass
+            else :
+                qs = qs.filter(author = self.request.user)
+        else :
+            qs = qs.none()
+
+        return qs
 
 class ImgViewSet(viewsets.ModelViewSet):
     
